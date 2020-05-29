3e0bda2ce9fe89e532a2277464b181b2218649aa
KangJianPing <k1152316061@outlook.com>
2020-05-29 11:47:39 +0800

    Initial commit

---
+++ app/src/main/java/com/AndroidCourse/Activitys/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.AndroidCourse.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.AndroidCourse.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
